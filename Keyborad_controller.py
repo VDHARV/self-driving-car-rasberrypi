7e4cce87ce90eabd3de67a7ed1bf071f28323a88
aryan51k <75057473+aryan51k@users.noreply.github.com>
2022-01-06 16:00:30 +0530

    Added the basic keyboard module

---
+++ Keyborad_controller.py
@@ -0,0 +1,28 @@
+import pygame
+
+def pygame_setup():
+    '''This Function helps to setup the pygame'''
+    pygame.init()
+    win = pygame.display.set_mode((100,100))
+
+def getKey(key):
+    '''This helps to detect the key sent by user'''
+    ans  = False
+    for eve in pygame.event.get(): pass
+    keyInput = pygame.key.get_pressed()
+    key_sent = getattr(pygame, 'K_{}'.format(key))
+    if keyInput [key_sent]:
+        ans = True    
+    return ans
+    
+def main():
+    '''This is used to drive the code'''
+    if getKey('a'):
+        print("A was pressed")
+    if getKey('b'):
+        print('B was pressed')
+
+if __name__ == "__main__":
+    pygame_setup()
+    while True:
+        main()
