7f1d12ffd9d1a9ca4f1f75b98abf8d39baa1f59a
aryan51k <75057473+aryan51k@users.noreply.github.com>
2022-01-22 11:04:59 +0530

    Complete code

--- Image_Processing/utils.py
+++ Image_Processing/utils.py
@@ -14,13 +14,15 @@ def thresholding(img):
     return masked
 
 
-def warp_img(img,points,w,h):
+def warp_img(img,points,w,h, inverse = False):
     '''This function takes in the image and points and provides the warped image'''
     #p1 are the points of the 4 red dots in the window they are basically the points on which the perspective change is required 
     pt1 = np.float32(points)
     pt2 = np.float32([[0,0], [w,0], [0,h], [w,h]])
-    
-    matrix = cv2.getPerspectiveTransform(pt1, pt2)
+    if inverse:
+        matrix = cv2.getPerspectiveTransform(pt2, pt1)
+    else:    
+        matrix = cv2.getPerspectiveTransform(pt1, pt2)
     
     img_warp = cv2.warpPerspective(img, matrix,(w,h))
     
@@ -60,24 +62,57 @@ def draw_points(img, points):
         
     return img
 
-def get_histogram(img, percent=0.1, display = False):
+def get_histogram(img, percent=0.1, display = False, region = 1):
     '''This function performs the pixel summation so that curve can be extracted '''
-
-    hist_values = np.sum(img, axis=0)
+    
+    if region == 1:
+        hist_values = np.sum(img, axis=0)
+    else:
+        hist_values = np.sum(img[img.shape[0] // region:,:], axis=0)
     #print(hist_values)
     max_val = np.max(hist_values)
     min_val = max_val*percent
     index_array = np.where(hist_values >= min_val)
     base_point = int(np.average(index_array))
     # the base point value suggests weather to turn right or left
-    print(base_point)
+    # print(base_point)
     if display:
         img_hist = np.zeros((img.shape[0], img.shape[1], 3), np.uint8)
         for x, intensity in enumerate(hist_values):
-            cv2.line(img_hist, (x, img.shape[0]), (x, int(img.shape[0] - intensity // 255)), (255, 0, 255), 1)
+            cv2.line(img_hist, (x, img.shape[0]), (x, int(img.shape[0] - intensity // 255 //region)), (255, 0, 255), 1)
             cv2.circle(img_hist, (base_point, img.shape[0]), 20, (0, 255, 255), cv2.FILLED)
         return base_point, img_hist
 
     return base_point
 
+def stack(scale,imgArray):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range ( 0, rows):
+            for y in range(0, cols):
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                else:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank]*rows
+        hor_con = [imageBlank]*rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            else:
+                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor= np.hstack(imgArray)
+        ver = hor
+    return ver
         
--- Image_Processing/video.py
+++ Image_Processing/video.py
@@ -2,9 +2,12 @@ import cv2
 import numpy as np
 from pygame import image 
 import utils
+curve_list = []
+avg_val = 10
 
-def get_curve(img):
+def get_curve(img, display = 2):
     '''This function returns the curve values'''
+    imgResult = img.copy()
     
     img_thres = utils.thresholding(img)
     h, w, c = img.shape
@@ -12,13 +15,59 @@ def get_curve(img):
     img_warp = utils.warp_img(img_thres, points, w, h)
     img_warped_points = utils.draw_points(img, points)
     
-    base_point, img_hist = utils.get_histogram(img_warp, display=True)
-    cv2.imshow("thres", img_thres)
-    cv2.imshow("wrap", img_warp)
-    cv2.imshow("warp points", img_warped_points)
-    cv2.imshow("warp points", img_hist)
+    mid_point, img_hist = utils.get_histogram(img_warp, display=True, percent=0.5, region=4)
+    curve_avg, img_hist = utils.get_histogram(img_warp, display=True, percent=0.9)
+    curve_raw = curve_avg - mid_point
     
-    return 
+    curve_list.append(curve_raw)
+    
+    if len(curve_list) > avg_val:
+        curve_list.pop(0)
+    
+    curve = int(sum(curve_list) / len(curve_list))
+    
+    ## Display fucntion 
+    if display != 0:
+       imgInvWarp = utils.warp_img(img_warp, points, w, h,inverse= True)
+       imgInvWarp = cv2.cvtColor(imgInvWarp,cv2.COLOR_GRAY2BGR)
+       imgInvWarp[0:h//3,0:w] = 0,0,0
+       imgLaneColor = np.zeros_like(img)
+       imgLaneColor[:] = 0, 255, 0
+       imgLaneColor = cv2.bitwise_and(imgInvWarp, imgLaneColor)
+       imgResult = cv2.addWeighted(imgResult,1,imgLaneColor,1,0)
+       midY = 450
+       cv2.putText(imgResult,str(curve),(w//2-80,85),cv2.FONT_HERSHEY_COMPLEX,2,(255,0,255),3)
+       cv2.line(imgResult,(w//2,midY),(w//2+(curve*3),midY),(255,0,255),5)
+       cv2.line(imgResult, ((w // 2 + (curve * 3)), midY-25), (w // 2 + (curve * 3), midY+25), (0, 255, 0), 5)
+       for x in range(-30, 30):
+           w = w // 20
+           cv2.line(imgResult, (w * x + int(curve//50 ), midY-10),
+                    (w * x + int(curve//50 ), midY+10), (0, 0, 255), 2)
+       #fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer);
+       #cv2.putText(imgResult, 'FPS '+str(int(fps)), (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (230,50,50), 3);
+    if display == 2:
+       imgStacked = utils.stack(0.7,([img,img_warped_points,img_warp],
+                                         [img_hist,imgLaneColor,imgResult]))
+       cv2.imshow('ImageStack',imgStacked)
+    
+    elif display == 1:
+       cv2.imshow('Resutlt',imgResult)
+    
+
+    # cv2.imshow("thres", img_thres)
+    # cv2.imshow("wrap", img_warp)
+    # cv2.imshow("warp points", img_warped_points)
+    # cv2.imshow("warp points", img_hist)
+    
+    curve = curve/100
+    # normalizing the curve 
+    
+    if curve > 1:
+        curve = 1
+    if curve < -1:
+        curve = -1
+    
+    return curve
     
 
 
@@ -36,6 +85,6 @@ if __name__ == "__main__":
         
         success, img = video.read()    
         img = cv2.resize(img, (480, 240))
-        get_curve(img)
+        curve = get_curve(img)
         cv2.imshow("Vid", img)
         cv2.waitKey(1)
